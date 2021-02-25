0b7c442d5cca7e775b3931bd7f7a55b5b7db8c05
Dieter <dieter.vanderelst@fastmail.net>
2021-02-25 12:01:14 -0500

    comminting before merging

--- Settings.py
+++ Settings.py
@@ -11,17 +11,12 @@ default_repeats = 5
 servo_port = 'COM4'
 sonar_port = 'COM5'
 
-servo_zero = 1220
-servo_range = [400, 2000]
+#servo_zero = 1220
+#servo_range = [400, 2000]
+servo_positions = numpy.linspace(200, 2500, 5)
 
-max_deviation = min(servo_zero - servo_range[0], servo_range[1] - servo_zero)
-min_position = servo_zero - max_deviation
-max_position = servo_zero + max_deviation
-
-servo_positions = numpy.linspace(min_position, max_position, 9)
-
-start_freq = 40001
-end_freq = 39999
+start_freq = 40000 +1
+end_freq = 40000 - 1
 samples = 100
 measurement_pause = 0.1
 servo_pause = 1
